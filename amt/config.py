5f586d37277ebd3481c85804089e993ec80e6ea0
Louis <Louisb19@live.co.uk>
2024-02-15 18:00:09 +0000

    add tokenizer

---
+++ amt/config.py
@@ -0,0 +1,23 @@
+"""Includes functionality for loading config files."""
+
+import os
+import json
+
+
+CONFIG_DIR = os.path.join(os.path.dirname(__file__), "..", "config")
+
+
+def load_config():
+    """Returns a dictionary loaded from the config.json file."""
+    with open(os.path.join(CONFIG_DIR, "config.json")) as f:
+        return json.load(f)
+
+
+def load_model_config(name: str):
+    """Returns a dictionary containing the model config."""
+    model_config_path = os.path.join(CONFIG_DIR, "models", f"{name}.json")
+    assert os.path.isfile(
+        model_config_path
+    ), f"Could not find config file for model={name} in config/models"
+    with open(model_config_path) as f:
+        return json.load(f)
