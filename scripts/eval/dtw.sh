0e6f910431b91d04fd4a1427f5dd4f45a3ee9c67
Louis <loua19@outlook.com>
2024-04-16 12:10:57 +0000

    clean up

--- scripts/eval/dtw.sh
+++
@@ -1,5 +0,0 @@
-python /home/loubb/work/aria-amt/scripts/eval/dtw.py \
-    -audio_dir /mnt/ssd1/data/mp3/raw/aria-mp3 \
-    -mid_dir /mnt/ssd1/amt/transcribed_data/0/aria-mid \
-    -output_file /mnt/ssd1/amt/transcribed_data/0/aria-mid.csv
-    
--- scripts/eval/mir.sh
+++
@@ -1,5 +0,0 @@
-python /home/loubb/work/aria-amt/amt/evaluate.py \
-    --est-dir /home/loubb/work/aria-amt/maestro-ft \
-    --ref-dir /mnt/ssd1/data/mp3/raw/maestro-mp3 \
-    --output-stats-file out.json
-    
