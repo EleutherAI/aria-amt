c0809c1a254fc65cad80d8147a20bc4b1a37b578
Louis <loua19@outlook.com>
2024-08-12 12:53:08 +0000

    reorganize readme scripts and config

---
+++ scripts/transcribe-youtube.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p data models
+[ -f data/audio.webm ] && rm data/audio.webm
+[ -f data/audio.mp3 ] && rm data/audio.mp3
+
+yt-dlp -x --audio-format mp3 --extract-audio --audio-quality 0 --no-playlist -o data/audio.mp3 $1
+
+aria-amt transcribe \
+    medium-double \
+    models/piano-medium-double-1.0.safetensors \
+    -load_path data/audio.mp3 \
+    -save_dir data \
+    -compile \
+    -bs 1
