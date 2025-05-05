5c24301c64989e85c52c99e5084180e1ad8c0f9b
Louis <Louisb19@live.co.uk>
2025-05-05 10:11:36 +0100

    Migrate to pyproject.toml (#63)
    * change packaging and fix audio bug
    
    * fix transcribe worker bug
    
    * fix toml
    
    * upgrade to python3.11

--- scripts/transcribe-youtube.sh
+++ scripts/transcribe-youtube.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 mkdir -p data models
-[ -f data/audio.webm ] && rm data/audio.webm
-[ -f data/audio.mp3 ] && rm data/audio.mp3
+[ -f data/audio.webm ] && rm ./data/audio.webm
+[ -f data/audio.mp3 ] && rm ./data/audio.mp3
 
-yt-dlp -x --audio-format mp3 --extract-audio --audio-quality 0 --no-playlist -o data/audio.mp3 $1
-
+yt-dlp -x --audio-format mp3 --extract-audio --audio-quality 0 --no-playlist -o ./data/audio.mp3 --match-filter "duration <= 3h" $1
+ 
 aria-amt transcribe \
     medium-double \
     models/piano-medium-double-1.0.safetensors \
-    -load_path data/audio.mp3 \
-    -save_dir data \
+    -load_path ./data/audio.mp3 \
+    -save_dir ./data \
     -compile \
     -bs 1
